19936f2e3f6f5c3e402c2ff4b75d0cf2068e61f6
Abdulkadir Genc <abdulkadirgenc@outlook.com>
2019-07-24 10:28:49 +0300

    repository restructured

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ng-wizard-app!');
+    expect(page.getTitleText()).toEqual('Welcome to demo!');
   });
 
   afterEach(async () => {
